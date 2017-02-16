c247ee234112e7db2c9219498fdac819b6619b74
B <bcm811@gmail.com>
2017-02-16 15:04:13 -0700

    got new CPIGroup php-amazon-mws library installed, figured out namespacing... remember to put a '\' in front of globally namespaced functions to access them outside of the controllers predefined namespace

--- app/Http/Controllers/AmazonController.php
+++ app/Http/Controllers/AmazonController.php
@@ -1,16 +1,26 @@
 <?php
-
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
-require_once(app_path() . '/Libraries/Amazon/src/MarketplaceWebService/Client.php');
-require_once(app_path() . '/Libraries/Amazon/src/MarketplaceWebService/Samples/.config.inc.php');
-        
-        use MarketplaceWebService_Client;
 
 class AmazonController extends Controller
-{
+{ 
+  public function getAmazonOrders() {
+    try {
+        $amz = new \AmazonOrderList("PROLINE"); //store name matches the array key in the config file
+        $amz->setLimits('Modified', "- 24 hours"); //accepts either specific timestamps or relative times 
+        $amz->setFulfillmentChannelFilter("MFN"); //no Amazon-fulfilled orders
+        $amz->setOrderStatusFilter(
+            array("Unshipped", "PartiallyShipped", "Canceled", "Unfulfillable")
+            ); //no shipped or pending orders
+        $amz->setUseToken(); //tells the object to automatically use tokens right away
+        $amz->fetchOrders(); //this is what actually sends the request
+        return $amz->getList();
+    } catch (Exception $ex) {
+        echo 'There was a problem with the Amazon library. Error: '.$ex->getMessage();
+    }
+}
 
     /**
     * Invokes the the library for MWS
@@ -19,7 +29,20 @@ class AmazonController extends Controller
     */
     public function service_test()
     {
-        
+        $list= $this->getAmazonOrders();
+        if ($list) {
+    echo 'My Store Orders<hr>';
+    foreach ($list as $order) {
+        //these are AmazonOrder objects
+        echo '<b>Order Number:</b> '.$order->getAmazonOrderId();
+        echo '<br><b>Purchase Date:</b> '.$order->getPurchaseDate();
+        echo '<br><b>Status:</b> '.$order->getOrderStatus();
+        echo '<br><b>Customer:</b> '.$order->getBuyerName();
+        $address=$order->getShippingAddress(); //address is an array
+        echo '<br><b>City:</b> '.$address['City'];
+        echo '<br><br>';
+    }
+}
     }
     /**
      * Display a listing of the resource.
